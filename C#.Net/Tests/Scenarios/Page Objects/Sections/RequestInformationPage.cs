1fa50a39f826c47824d7b60b6becd0ba7cea1bac
Roman Soroka1 <Roman_Soroka1@epam.com>
2016-05-19 12:04:15 +0300

    The new 2 tests for the demonstration purposes were added: AboutTests, PresentationTes. Pages and Section for those tests were added. Epam site structure was modified.

---
+++ C#.Net/Tests/Scenarios/Page Objects/Sections/RequestInformationPage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Epam.JDI.Core.Interfaces.Complex;
+using Epam.JDI.Web.Attributes;
+using Epam.JDI.Web.Selenium.Elements.Base;
+using Epam.JDI.Web.Selenium.Elements.Common;
+using Epam.JDI.Web.Selenium.Elements.Complex;
+using Epam.JDI.Web.Selenium.Elements.Composite;
+using Epam.Tests.Scenarios.Enums;
+using Epam.Tests.Scenarios.Page_Objects.Sections;
+
+namespace Epam.Tests.Scenarios.Page_Objects.Pages
+{
+    public class RequestInformationPage : WebPage
+    {
+        [FindBy(Css = "textarea[name*=\"user_comment_how_hear_about\"]")]
+        public TextArea MessageArea;
+    }
+}
