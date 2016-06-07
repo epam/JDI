2f9b0c6faa977922dda4337ec2cf8edb5d8e16a7
vitalii_sokolov@epam.com <vertin2@gmail.com>
2016-06-07 16:50:55 +0300

    added contactform

--- TestApplications/Android/JDITestAndroidApp/app/src/main/java/com/epam/jdi/jditestandroidapp/ui/ContactFormFragment.java
+++ TestApplications/Android/JDITestAndroidApp/app/src/main/java/com/epam/jdi/jditestandroidapp/ui/ContactFormFragment.java
@@ -7,7 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.epam.jdi.jditestandroidapp.R;
 import com.epam.jdi.jditestandroidapp.base.BackFragment;
@@ -22,6 +25,12 @@ public class ContactFormFragment extends BackFragment implements View.OnClickLis
 
     RadioGroup mLeft;
     RadioGroup mRight;
+
+    EditText mName;
+    EditText mLastName;
+    EditText mDescr;
+
+    TextView mResult;
 
     public static Fragment newInstance() {
         return new ContactFormFragment();
@@ -41,6 +50,10 @@ public class ContactFormFragment extends BackFragment implements View.OnClickLis
         mCalculate = (Button) view.findViewById(R.id.calculate_btn);
         mLeft = (RadioGroup) view.findViewById(R.id.radio1);
         mRight = (RadioGroup) view.findViewById(R.id.radio2);
+        mResult = (TextView) view.findViewById(R.id.result);
+        mName = (EditText) view.findViewById(R.id.input_name);
+        mLastName = (EditText) view.findViewById(R.id.input_last_name);
+        mDescr = (EditText) view.findViewById(R.id.input_descr);
 
         mSubmit.setOnClickListener(this);
         mCalculate.setOnClickListener(this);
@@ -49,10 +62,15 @@ public class ContactFormFragment extends BackFragment implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.submit_btn) {
+        RadioButton r1 = (RadioButton) getView().findViewById(mLeft.getCheckedRadioButtonId());
+        int r1Value = Integer.parseInt(r1.getText().toString());
+        RadioButton r2 = (RadioButton) getView().findViewById(mRight.getCheckedRadioButtonId());
+        int r2Value = Integer.parseInt(r2.getText().toString());
 
-        } else {
-
-        }
+        mResult.setText(getString(R.string.contact_form_result_format,
+                mName.getText(),
+                mLastName.getText(),
+                mDescr.getText(),
+                r1Value + r2Value));
     }
 }
