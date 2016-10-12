00c76b8d579c9d7740ea2502a132edbeb7ebd4a6
Aleksei_Voronin <Aleksei_Voronin@epam.com>
2016-10-12 15:57:43 +0300

    add Matcher's tests fix TestBase namespace

--- C#.Net/Tests/JDI Tests/Tests/Epam/FormExamples.cs
+++ C#.Net/Tests/JDI Tests/Tests/Epam/FormExamples.cs
@@ -23,7 +23,7 @@ namespace JDI_Tests.Tests.Epam
         public void FillFormExample(Attendee attendee)
         {
             EpamSite.JobDescriptionPage.AddCvForm.Submit(attendee);
-            // It's not stable, GetAttribute() sometimes returnes all classes and sometimes only a part 'required '
+            // It's not stable, GetAttribute() sometimes returnes all classes and sometimes only a first part 'required '
             Assert.That(EpamSite.JobDescriptionPage.Captcha.GetAttribute("class"), Does.Contain("form-field-error"));
         }
     }
