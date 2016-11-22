648cc24839fd76d21c72aa74b31518b0b64863f2
Aleksei_Voronin <ralexeyr@gmail.com>
2016-11-22 12:29:47 +0300

    [C#] add CheckBox tests

---
+++ C#.Net/Tests/JDI UIWebTests/JDI UIWebTests/DataProviders/CheckBoxProvider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace JDI_UIWebTests.DataProviders
+{
+    class CheckBoxProvider
+    {
+        public static IEnumerable InputData
+        {
+            get
+            {
+                yield return new TestCaseData("true", true);
+                yield return new TestCaseData("1", true);
+                yield return new TestCaseData("false", false);
+                yield return new TestCaseData("0", false);
+            }
+        }
+
+        public static IEnumerable<string> InputValue
+        {
+            get
+            {
+                yield return "true ";
+                yield return "1 ";
+                yield return " false";
+                yield return "0 ";
+                yield return " ";
+                yield return "123";
+                yield return " 1";
+                yield return " 0";
+                yield return "!@#$%^&*";
+                yield return "qwdewf";
+                yield return "1qwe";
+                yield return "true123";
+                yield return "123true";
+                yield return "false123";
+                yield return "123false";
+                yield return "o";
+                yield return "O";
+                yield return "tr ue";
+            }
+        }
+    }
+}
