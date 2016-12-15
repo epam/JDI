0136ff4e25d99ddf00e030faf1ad20cf7ec1b71e
Roman <viqablog@gmail.com>
2016-12-15 10:45:59 +0300

    remove unused imports

--- Java/Tests/jdi-uitest-tutorialtests/src/test/java/com/epam/jdi/uitests/testing/career/common/tests/TestsBase.java
+++ Java/Tests/jdi-uitest-tutorialtests/src/test/java/com/epam/jdi/uitests/testing/career/common/tests/TestsBase.java
@@ -2,15 +2,11 @@ package com.epam.jdi.uitests.testing.career.common.tests;
 
 import com.epam.jdi.uitests.testing.career.page_objects.site.EpamSite;
 import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
-import com.epam.jdi.uitests.web.settings.WebSettings;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import static com.epam.jdi.uitests.core.settings.JDISettings.logger;
-import static com.epam.jdi.uitests.core.settings.JDISettings.useDriver;
 import static com.epam.jdi.uitests.web.selenium.driver.WebDriverUtils.killAllRunWebDrivers;
 
 /**
