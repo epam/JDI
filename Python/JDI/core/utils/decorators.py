190e8b6a93b930d76dd3cbc7eb93234477bcf402
Natalia_Grebenshchikova <nvgrebenshchikova@gmail.com>
2017-07-04 16:11:33 +0300

    correct element's attribute in log message

--- Python/JDI/core/utils/decorators.py
+++ Python/JDI/core/utils/decorators.py
@@ -4,24 +4,29 @@ from JDI.core.settings.jdi_settings import log
 from JDI.web.selenium.settings.WebSettings import WebSettings
 
 
+def action_populated_string(action_name, values_list, el):
+    a2 = action_name
+    for value in list(values_list):
+        if value not in {"value_from_function", "2_values_from_function"}:
+            val = getattr(el[0], value)
+            val = val if val is not None else "not named"
+            a2 = str.replace(a2, "%s", val, 1)
+        elif value == "value_from_function":
+            a2 = str.replace(a2, "%s", str(el[1]))
+        elif value == "2_values_from_function":
+            a2 = str.replace(str.replace(a2, "%s", str(el[1]), 1), "%s", str(el[2]))
+    return a2
+
+
 def action_logging_start(action_name, values_list, el):
     if log.to_do_info_logging:
-        a2 = action_name
-        for value in values_list:
-            if value not in {"value_from_function", "2_values_from_function"}:
-                val = getattr(el[0], value)
-                val = val if val is not None else "not named"
-                a2 = str.replace(a2, "%s", val, 1)
-            elif value == "value_from_function":
-                a2 = str.replace(a2, "%s", str(el[1]))
-            elif value == "2_values_from_function":
-                a2 = str.replace(str.replace(a2, "%s", str(el[1]), 1), "%s", str(el[2]))
-        WebSettings.logger.info("{0} '{1}' '{2}' ({3})".format(a2, el[0].__class__.__name__, el[0].name, str(el[0])))
+        WebSettings.logger.info("{0} '{1}' '{2}' ({3})".format(action_populated_string(action_name, values_list, el),
+                                                               el[0].__class__.__name__, el[0].name, str(el[0])))
 
 
-def action_logging_stop(action_name):
+def action_logging_stop(action_name, values_list, el):
     if log.to_do_info_logging:
-        WebSettings.logger.info("%s correctly done" % action_name)
+        WebSettings.logger.info("%s correctly done" % action_populated_string(action_name, values_list, el))
 
 
 def scenario(action_name, values_list={}):
@@ -42,9 +47,9 @@ def scenario(action_name, values_list={}):
                         time2.sleep(0.5)
                         t += 1
                         ex = exception
-                    finally:
+                    finally: 
                         if done:
-                            action_logging_stop(action_name)
+                            action_logging_stop(action_name, values_list, el)
             finally:
                 if ex is not None and log.to_do_info_logging:
                     WebSettings.logger.error(ex)
