415ea7d748c04bf1da9a09bc31d42ba5861945b7
Sergei_Petrov1 <shadow170713@gmail.com>
2017-08-28 19:05:53 +0300

    Revert "EPMLSTRTAI-2. Delete mock-rest from logger."
    This reverts commit 5bac17a

---
+++ Java/JDI/jdi-mock-rest/src/main/java/com/epam/mock/model/Method.java
@@ -0,0 +1,8 @@
+package com.epam.mock.model;
+
+/**
+ * Enumeration with all possible method names.
+ */
+public enum Method {
+    GETUSERS, GETUSER, REGISTERUSER, UPDATEUSER, UPDATEUSERFIELD, CLEARUSER, CLEARALL
+}
