fe1f6911596123abe8f16cf7a6717283c0cd7f70
lukyanov <serjluk4@gmail.com>
2017-09-08 17:55:00 +0300

    Dozer mapper added, entities refactored partially, Step entity created

---
+++ Java/JDI/jdi-cucumber-test-generator/src/main/java/com/epam/test_generator/DozerMapper.java
@@ -0,0 +1,41 @@
+package com.epam.test_generator;
+
+import com.epam.test_generator.dto.StepDTO;
+import com.epam.test_generator.entities.Step;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DozerMapper {
+
+    private static Mapper mapper;
+
+    static {
+        List cfg = new ArrayList();
+        cfg.add("dozerMapping.xml");
+        mapper = new DozerBeanMapper(cfg);
+    }
+
+    public static void map(Object source, Object dest) {
+        mapper.map(dest, source);
+    }
+
+    public static void main(String[] args) {
+        Step dest = new Step();
+        StepDTO src = new StepDTO();
+
+        src.setId(1L);
+        src.setRowNumber(1);
+        src.setDescription("description of step");
+
+
+
+        System.out.println("before: " + dest);
+        DozerMapper.mapper.map(src, dest);
+        System.out.println("after: " + dest);
+    }
+
+
+}
