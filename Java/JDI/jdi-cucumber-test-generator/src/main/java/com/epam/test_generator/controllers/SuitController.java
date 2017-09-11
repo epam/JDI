6983b11cd10c0eb556e866cf6e4c1a19cff3e2a2
Lomdji <lomdjaria11@gmail.com>
2017-09-11 17:34:25 +0300

    EPMLSTRTAI-44,Added opportunity to generate FeatureFiles

--- Java/JDI/jdi-cucumber-test-generator/src/main/java/com/epam/test_generator/controllers/SuitController.java
+++ Java/JDI/jdi-cucumber-test-generator/src/main/java/com/epam/test_generator/controllers/SuitController.java
@@ -2,16 +2,19 @@ package com.epam.test_generator.controllers;
 
 import com.epam.test_generator.entities.Suit;
 import com.epam.test_generator.services.SuitService;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SuitController {
@@ -19,45 +22,62 @@ public class SuitController {
     @Autowired
     SuitService suitService;
 
-	@RequestMapping(value = "/")
-    public String getMainPage(){
+    @RequestMapping(value = "/")
+    public String getMainPage() {
         return "/WEB-INF/static/views/newSuits";
     }
 
     @RequestMapping(value = "/getTestSuits", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody List<Suit> getSuits() {
+    public @ResponseBody
+    List<Suit> getSuits() {
         return suitService.getSuits();
     }
 
     @RequestMapping(value = "/getSuit/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public Suit getSuit(@PathVariable("id") long id){
+    public Suit getSuit(@PathVariable("id") long id) {
         return suitService.getSuit(id);
     }
 
     @RequestMapping(value = "/getSuitByName/{name}", method = RequestMethod.GET)
     @ResponseBody
-    public Suit getSuitByName(@PathVariable("name") String name){
+    public Suit getSuitByName(@PathVariable("name") String name) {
         return suitService.getSuitByName(name);
     }
 
-    @RequestMapping(value="/editTestSuit", method = RequestMethod.POST, consumes = "application/json")
-    public ResponseEntity<Void> editSuit(@RequestBody Suit suit){
+    @RequestMapping(value = "/editTestSuit", method = RequestMethod.POST, consumes = "application/json")
+    public ResponseEntity<Void> editSuit(@RequestBody Suit suit) {
         suitService.editSuit(suit);
-      
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping(value = "/removeTestSuit/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Void> removeSuit(@PathVariable("id") long id){
+    public ResponseEntity<Void> removeSuit(@PathVariable("id") long id) {
         suitService.removeSuit(id);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @RequestMapping(value="/addTestSuit", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    @RequestMapping(value = "/addTestSuit", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     @ResponseBody
     public Suit addSuit(@RequestBody Suit suit) {
         return suitService.addSuit(suit);
+    }
+
+    @RequestMapping(value = "/downloadFeatureFile", method = RequestMethod.GET)
+    public void downloadFile(@RequestParam(name = "suitId") Long suitId,
+                             @RequestParam(name = "caseIds") List<Long> caseIds,
+                             HttpServletResponse response) throws IOException {
+
+        String mimeType = "application/octet-stream";
+        System.out.println("mimetype : " + mimeType);
+        response.setContentType(mimeType);
+        response.setHeader("Content-Disposition", "inline; filename=\"" + "File.feature" + "\"");
+
+        ByteArrayInputStream inputStream = suitService.generateStream(suitId, caseIds);
+        response.setContentLength(inputStream.available());
+        FileCopyUtils.copy(inputStream, response.getOutputStream());
+        inputStream.close();
     }
 }
--- Java/JDI/jdi-cucumber-test-generator/src/main/java/com/epam/test_generator/services/SuitService.java
+++ Java/JDI/jdi-cucumber-test-generator/src/main/java/com/epam/test_generator/services/SuitService.java
@@ -1,10 +1,12 @@
 package com.epam.test_generator.services;
 
 import com.epam.test_generator.dao.interfaces.SuitDAO;
+import com.epam.test_generator.entities.Case;
 import com.epam.test_generator.entities.Suit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
 import java.util.List;
 
 @Service
@@ -35,5 +37,36 @@ public class SuitService {
 
     public Suit getSuitByName(String name) {
         return suitDAO.getSuitByName(name);
+    }
+
+    public ByteArrayInputStream generateStream(Long suitId, List<Long> CaseIds) throws IOException {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Suit suit = suitDAO.findOne(suitId);
+
+        try (PrintWriter writer = new PrintWriter(stream)) {
+            writer.print("Feature: ");
+            writer.print(suit.getName());
+            writer.println();
+            for (Case caze : suit.getCases()) {
+                if (CaseIds.contains(caze.getId())) {
+                    writer.print("Scenario: ");
+                    writer.print(caze.getDescription());
+                    writer.println();
+//            For new Step from branch 112
+//
+//            for (Step step: caze.getSteps()) {
+//                buf = new StringBuilder(step.getKeyword());
+//                buf.append(" ");
+//                buf.append(step.getDescription);
+//                outputFile.write(buf.toString());
+//                outputFile.newLine();
+//            }
+                }
+            }
+        }
+
+        stream.close();
+        return new ByteArrayInputStream(stream.toByteArray());
     }
 }
