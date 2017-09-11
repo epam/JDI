53b13e2e8e09a07a782a8a1ccc137f9445b6d2b9
pochanin <roma_02.97@mail.ru>
2017-09-11 11:42:39 +0300

    EPMLSTRTAI-40: EPMLSTRTAI-86: Table with steps was added.

---
+++ Java/JDI/jdi-cucumber-test-generator/src/main/webapp/WEB-INF/static/js/buttonsExecuting.js
@@ -0,0 +1,112 @@
+function cancelCaseEditing() {
+    $.get("/getCase/" + $("#caseId").text(), function(response){
+        $("#code-textarea").val(response.steps);
+        $("#case-description-textfield").val(response.description);
+        $("#case-priority-textfield").val(response.priority);
+    });
+}
+
+function saveCase() {
+    var description = $("#case-description-textfield").val();
+//    var priority = $("#case-priority-textfield").val();
+    var code = $("#code-textarea").val();
+    var suit_id = $("#suitId").text();
+    var case_id = $("#caseId").text();
+
+    // alert(description);
+    // alert(priority);
+    // alert(code);
+    // alert(case_id);
+
+    if (description === null || description === "") {
+            alert("Fill in all required entry field!");
+            return;
+        }
+
+
+    var formData = {
+        "id": case_id,
+        "description": description,
+        "steps": code
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/updateCase",
+        contentType : 'application/json',
+        data: JSON.stringify(formData),
+        success : function(response) {
+            getSuitInfoWithOutCleanCases($(".is-active").text());
+        },
+        error: function( xhr, textStatus ) {
+            alert( [ xhr.status, textStatus ] );
+        }
+    });
+}
+
+function removeCases() {
+    var description = $("#case-description-textfield").val();
+    var priority = $("#case-priority-textfield").val();
+    var code = $("#code-textarea").val();
+
+
+    if (description === null || description === "" ||
+        priority === null || priority === "") {
+        alert("Fill in all required entry field!");
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/saveCase",
+        data: {
+            suitID: suit_id,
+            caseId: case_id,
+            description: description,
+            priority: priority,
+            code: code
+        }, // parameters
+        success : function(response) {
+            getSuitInfo($(".is-active").text());
+        },
+        error: function( xhr, textStatus ) {
+            alert( [ xhr.status, textStatus ] );
+        }
+    });
+}
+
+function disableCaseButtons () {
+    var flag = false;
+
+    $('#tableCases input:checkbox').each(function() {
+        if ($(this).prop("checked")) {
+            flag = true;
+        }
+    });
+
+    if (!flag) { // если ни один checkbox не отмечен
+        $(".delete-cases-button").addClass("disabled-link");
+        $(".generate-feature-button").addClass("disabled-link");
+    } else {
+        $(".delete-cases-button").removeClass("disabled-link");
+        $(".generate-feature-button").removeClass("disabled-link");
+    }
+}
+
+$("#tableCases").on("change", "input", function(){
+    var flag = false;
+
+    $('#tableCases input:checkbox').each(function() {
+        if ($(this).prop("checked")) {
+            flag = true;
+        }
+    });
+
+    if (!flag) { // если ни один checkbox не отмечен
+        $(".delete-cases-button").addClass("disabled-link");
+        $(".generate-feature-button").addClass("disabled-link");
+    } else {
+        $(".delete-cases-button").removeClass("disabled-link");
+        $(".generate-feature-button").removeClass("disabled-link");
+    }
+});
---
+++ Java/JDI/jdi-cucumber-test-generator/src/main/webapp/WEB-INF/static/js/suit/suitMain.js
@@ -0,0 +1,92 @@
+ var suit_id = -1;
+
+$(document).ready(function () {
+    $('.accordion-tabs').children('li').first().children('div').addClass('is-active').next().addClass('is-open').show();
+    $('.accordion-tabs').on('click', 'li > div', function(event) {
+        if (!$(this).hasClass('is-active')) {
+            event.preventDefault();
+            $('.accordion-tabs .is-open').removeClass('is-open').hide();
+            $(this).next().toggleClass('is-open').toggle();
+            $('.accordion-tabs').find('.is-active').removeClass('is-active');
+            $(this).addClass('is-active');
+        } else {
+            event.preventDefault();
+        }
+    });
+
+    $("#tableCases").on("click", "tr", function(){
+        $("#tableCases").children("tr").removeClass("is_active");
+        $(this).addClass("is_active");
+        $("#caseId").text($(this).children("td.small_td").children(".particular_caseId").val());
+        $("#code-textarea").val("");
+        $("#case-description-textfield").val("");
+        $("#case-priority-textfield").val("");
+        $.get("/getCase/" + $("#caseId").text(), function(response){
+            $("#code-textarea").val(response.steps);
+            $("#case-description-textfield").val(response.description);
+            $("#case-priority-textfield").val(response.priority);
+        });
+    });
+});
+
+function getSuitInfo(suit_name){
+    $.get("/getSuitByName/" + suit_name, function(response){
+        $("#suitId").text(response.id);
+        $("#nameSuit").text(response.name);
+        $("#descriptionSuit").text(response.description);
+        $("#countCases").text(response.cases.length);
+        $("#code-textarea").val("");
+        $("#case-description-textfield").val("");
+        $("#tableCases").empty();
+        for(var i = 0; i < response.cases.length; i++){
+            $("#tableCases").append($('<tr>')
+                                .append($('<td>')
+                                    .addClass('small_td')
+                                    .append($('<input>')
+                                        .attr('type', 'checkbox')
+                                    )
+                                    .append($('<input>')
+                                        .addClass('particular_caseId')
+                                        .attr('type', 'hidden')
+                                        .val(response.cases[i].id)
+                                    )
+                                )
+                                .append($('<td>')
+                                    .text(response.cases[i].description)
+                                )
+                            );
+        }
+    });
+
+    disableCaseButtons();
+}
+
+function getSuitInfoWithOutCleanCases(suit_name){
+    $.get("/getSuitByName/" + suit_name, function(response){
+        $("#suitId").text(response.id);
+        $("#nameSuit").text(response.name);
+        $("#descriptionSuit").text(response.description);
+        $("#countCases").text(response.cases.length);
+        $("#tableCases").empty();
+        for(var i = 0; i < response.cases.length; i++){
+            $("#tableCases").append($('<tr>')
+                                .append($('<td>')
+                                    .addClass('small_td')
+                                    .append($('<input>')
+                                        .attr('type', 'checkbox')
+                                    )
+                                    .append($('<input>')
+                                        .addClass('particular_caseId')
+                                        .attr('type', 'hidden')
+                                        .val(response.cases[i].id)
+                                    )
+                                )
+                                .append($('<td>')
+                                    .text(response.cases[i].description)
+                                )
+                            );
+        }
+    });
+
+    disableCaseButtons();
+}
