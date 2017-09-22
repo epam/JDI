cb1bb070425bdb1603f40f8f9ffbe7e7bdfd0768
Arsenii_Nazarov <arsnazarov94@gmail.com>
2017-09-22 20:28:35 +0300

    EPMLSTRTAI-87: adding and removing on suggestion page, tabs with categories

--- Java/JDI/jdi-cucumber-test-generator/src/main/webapp/WEB-INF/static/js/stepSuggestion.js
+++ Java/JDI/jdi-cucumber-test-generator/src/main/webapp/WEB-INF/static/js/stepSuggestion.js
@@ -1,71 +1,45 @@
-$(document).ready(function(){
-    getStepSuggestions();
-});
-
-function getStepSuggestions() {
-    axios.get("/cucumber/step_suggestion").then(function(response) {
-        $("#steps_table").empty();
-        $("#steps_table").append($('<tr>')
-                                .append($('<th>').text("Step content"))
-                                .append($('<th>').addClass("small_th"))
-                            );
-        for (var i = 0; i < response.data.length; i++) {
-            if(response.data[i].type == app.active){
-                $("#steps_table").append($('<tr>')
-                    .append($('<td>')
-                        .append(response.data[i].content)
-                    )
-                    .append($('<td>')
-                        .append("<button class='delete_step_button' onclick='deleteSuggestionStep(" + response.data[i].id + ")'>Delete step</button>")
-                    )
-                )
-            }
-        }
-    });
+"use strict";
+var Store = {
+    suggestion_steps: [],
+    active: 0,
 }
-
-function deleteSuggestionStep(stepId){
-    axios.delete('/cucumber/step_suggestion/' + stepId).then(function(response) {
-        getStepSuggestions();
-        successInfoBlock();
-    }).catch(function(error) {
-        errorInfoBlock("Fail deleting! Try again later!");
-    });
-}
-
-//function addSuggestionStep(){
-//    var text = $("#step_suggestion").val();
-//    $("#step_suggestion").val("");
-//
-//    axios.post('/cucumber/step_suggestion', {content: text}).then(function(response) {
-//        getStepSuggestions();
-//        savedSteps.getSavedSteps();
-//        successInfoBlock();
-//    }).catch(function(error) {
-//        errorInfoBlock("Fail adding! Try again later!");
-//    });
-//}
 
 var app = new Vue({
     el: "#app",
     data: {
-        active: 0,
+        Store,
         text: ""
     },
     methods:{
         computedClass: function (tab_active) {
-             return this.active == tab_active;
+             return Store.active == tab_active;
         },
         changeActiveTab: function (tab_active) {
-             this.active = tab_active;
-             getStepSuggestions();
+             Store.active = tab_active;
         },
         addSuggestionStep: function() {
-            axios.post('/cucumber/step_suggestion', {content: this.text, type: this.active}).then(function(response) {
-                getStepSuggestions();
+            var obj = {content: this.text, type: Store.active};
+            axios.post('/cucumber/step_suggestion', obj).then(function(response) {
+                Store.suggestion_steps.push(obj);
             });
             this.text = "";
+        },
+        deleteSuggestion: function(suggestion_id) {
+            axios.delete('/cucumber/step_suggestion/' + suggestion_id).then(function(response) {
+                axios.get("/cucumber/step_suggestion").then(function(response) {
+                    Store.suggestion_steps = response.data;
+                });
+            });
+        }
+    },
+    computed:{
+        getFiltredSteps: function() {
+            return Store.suggestion_steps.filter(function(step) {
+                return step.type == Store.active;
+            });
         }
     }
 });
-
+axios.get("/cucumber/step_suggestion").then(function(response) {
+    Store.suggestion_steps = response.data;
+});
