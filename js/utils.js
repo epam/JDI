825a95c503ecc1e90131913e8819351d783d911d
roman_iovlev <roman_iovlev@epam.com>
2017-12-09 17:28:12 -0500

    fix utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,19 @@
 var pages = ['index.html', 'contacts.html', 'support.html', 'dates.html', 'complex-table.html', 'simple-table.html', 'user-table.html', 'table-pages.html', 'different-elements.html', 'metals-colors.html']
+var num = -1;
+var linum = -1;
+var testLocArr = '';
+var testLoc = '';
 
 var data = {};
 var sliderCheck = {};
 
 $(document).ready(function () {
+	getPageState();
     checkLogin();
-    initPaging();
+	initLeftNavMenu();
+    if ($('.page-list-replace')) {
+		initPaging();
+	}
     reformTitle('salad');
 
 
@@ -351,62 +359,41 @@ function reformTitle(cname) {
 
     }
 }
-
-function initPaging() {
-
-    var testLocArr = location.pathname.split('/');
-    var testLoc = '';
+function getPageState() {	
+    testLocArr = location.pathname.split('/');
     if (testLocArr.length > 0)
         testLoc = testLocArr[testLocArr.length - 1];
     else testLoc = location.pathname;
-
-    var num = pages.indexOf(testLoc);
-    var linum = (num < 2) ? num : num + 1;
+    num = pages.indexOf(testLoc);
+    linum = (num < 2) ? num : num + 1;
+}
+function initLeftNavMenu() {
     $('.sidebar-menu li').removeClass('active');
     $($('.sidebar-menu li')[linum]).addClass('active');
-    if (linum > 2 && linum <10) {
+    if (linum > 2 && linum <pages.length) {
         $($('.sidebar-menu li')[2]).addClass('active');
         $('.sidebar-menu li .sub').show();
     }
-    if ($('.page-list-replace')) {
-		var paginator = '<ul class="uui-pagination"><li class="prev"><a href="#"><i class="fa fa-long-arrow-left"></i></a></li><li class="first"><a href="contacts.html">First</a></li>'
-		for (var i = 1; i < pages.length; i++) {
-			var str = (i == num) ? ' class="active"' : '';
-			paginator += '<li' + str + '><a href="' + pages[i].substring(1, pages[i].length) + '">' + i + '</a></li>'
-		}
-		paginator += '<li class="last"><a href="metals-colors.html">Last</a></li> <li class="next"><a href="index.html"><i class="fa fa-long-arrow-right"></i></a></li> </ul>'
-		$('.page-list-replace').replaceWith(paginator);		
-        if (num == 1) {
-            $('.prev').addClass('disable');
-            $('.first').addClass('disable');
-        } else if (num == pages.length - 1) {
-            $('.next').addClass('disable');
-            $('.last').addClass('disable');
-        }
-        if (num != pages.length - 1) $('.next:not(.disable) a').attr('href', pages[num + 1]);
-        if (num > 1) $('.prev:not(.disable) a').attr('href', pages[num - 1]);
-        $('.first:not(.disable) a').attr('href', pages[1]);
-        $('.last:not(.disable) a').attr('href', pages[pages.length - 1]);
+}
+function initPaging() {
+	var paginator = '<ul class="uui-pagination"><li class="prev"><a href="#"><i class="fa fa-long-arrow-left"></i></a></li><li class="first"><a href="contacts.html">First</a></li>'
+	for (var i = 1; i < pages.length; i++) {
+		var str = (i == num) ? ' class="active"' : '';
+		paginator += '<li' + str + '><a href="' + pages[i].substring(1, pages[i].length) + '">' + i + '</a></li>'
 	}
-    if ($('.uui-pagination')) {
-        if (num == 1) {
-            $('.prev').addClass('disable');
-            $('.first').addClass('disable');
-        } else if (num == pages.length - 1) {
-            $('.next').addClass('disable');
-            $('.last').addClass('disable');
-        }
-		var pagesList = '';
-        for (var i = 1; i < pages.length; i++) {
-            var str = (i == num) ? ' class="active"' : '';
-			pagesList = pagesList + '<li' + str + '><a href="' + pages[i].substring(1, pages[i].length) + '">' + i + '</a></li>'
-        }
-		$('.page-list-replace').replaceWith(pagesList);
-        if (num != pages.length - 1) $('.next:not(.disable) a').attr('href', pages[num + 1]);
-        if (num > 1) $('.prev:not(.disable) a').attr('href', pages[num - 1]);
-        $('.first:not(.disable) a').attr('href', pages[1]);
-        $('.last:not(.disable) a').attr('href', pages[pages.length - 1]);
-    }
+	paginator += '<li class="last"><a href="metals-colors.html">Last</a></li> <li class="next"><a href="index.html"><i class="fa fa-long-arrow-right"></i></a></li> </ul>'
+	$('.page-list-replace').replaceWith(paginator);		
+	if (num == 1) {
+		$('.prev').addClass('disable');
+		$('.first').addClass('disable');
+	} else if (num == pages.length - 1) {
+		$('.next').addClass('disable');
+		$('.last').addClass('disable');
+	}
+	if (num != pages.length - 1) $('.next:not(.disable) a').attr('href', pages[num + 1]);
+	if (num > 1) $('.prev:not(.disable) a').attr('href', pages[num - 1]);
+	$('.first:not(.disable) a').attr('href', pages[1]);
+	$('.last:not(.disable) a').attr('href', pages[pages.length - 1]);
 }
 
 function calculateValue() {
