3abef6a405c2ab9d1dd2528893b2637401043211
roman_iovlev <roman_iovlev@epam.com>
2017-12-27 20:18:35 +0300

    add id to logo

--- js/utils.js
+++ js/utils.js
@@ -384,7 +384,7 @@ function includeHeader() {
             <div class="epam-logo">\
 				<a href="/JDI/index.html">\
 					<span>Information<br>Framework</span>\
-					<img src="images/Logo_Epam_Color.svg" alt="ALT" width="86" />\
+					<img src="images/Logo_Epam_Color.svg" alt="ALT" id="epam_logo" width="86" />\
 				</a>\
             </div>\
 			<ul class="uui-navigation nav navbar-nav m-l8">\
