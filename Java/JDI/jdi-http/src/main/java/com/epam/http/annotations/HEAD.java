7e1a5c1ddad5b25633c2600354323035b3a583ff
Margarita Ivanova <margarita_ivanova@epam.com>
2019-03-12 14:40:38 +0300

    Added annotations for new methods

---
+++ Java/JDI/jdi-http/src/main/java/com/epam/http/annotations/HEAD.java
@@ -0,0 +1,12 @@
+package com.epam.http.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface HEAD {
+    String value();
+}
